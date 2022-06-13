86524f7aeba564c718b7e09c49a0bef420b40b27
sajjad <sajjadurrahman3434@gmail.com>
2022-06-13 11:41:54 +0600

    Final Version

--- src/utils/dayDifferenceCalculate.js
+++ src/utils/dayDifferenceCalculate.js
@@ -5,17 +5,17 @@
 
 import { ErrorLog } from "../helper/log";
 
-export const dayDifferenceCalculate = (endDate, startDate) => {
+export const dayDifferenceCalculate = (toDate, fromDate) => {
     let result = 0;
-    if (endDate && startDate) {
+    if (toDate && fromDate) {
         try {
-            result = (endDate.getTime() - startDate.getTime()) / (1000 * 3600 * 24);
+            result = (toDate.getTime() - fromDate.getTime()) / (1000 * 3600 * 24);
 
         } catch (err) {
             ErrorLog(err);
         }
     } else {
-        ErrorLog("dayDifferenceCalculate()= daydiff invalid: date validation failed for startdate or enddate)");
+        ErrorLog("dayDifferenceCalculate()= daydiff invalid: date validation failed for fromDate or toDate)");
         throw new Error(
             "Invalid Start or End Date"
         );
--- src/utils/rentalFeeCalculate.js
+++ src/utils/rentalFeeCalculate.js
@@ -5,17 +5,17 @@
 
 import { ErrorLog } from "../helper/log";
 
-export const rentalFeeCalculate = (price, dayDiff) => {
+export const rentalFeeCalculate = (priceOfProduct, dayDifference) => {
   let result = 0;
-  if (dayDiff && price) {
+  if (dayDifference && priceOfProduct) {
     try {
-      result = dayDiff * price;
+      result = dayDifference * priceOfProduct;
     } catch (err) {
       ErrorLog(err);
     }
   } else {
-    ErrorLog("rentalFeeCalculate(): dayDiff or price is missing");
-    throw new Error("rentalFeeCalculation(): dayDiff or price is missing");
+    ErrorLog("rentalFeeCalculate(): dayDifference or priceOfProduct is missing");
+    throw new Error("rentalFeeCalculation(): dayDifference or priceOfProduct is missing");
   }
 
   return result;
